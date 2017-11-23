6c468c990802fac4a20e717468c126fe9683f69a
quanglinh <linhtlqps05814@fpt.edu.vn>
2017-11-23 16:50:24 +0700

    new

--- app/src/androidTest/java/com/example/dell/lab4/hhhh.java
+++
@@ -1,9 +0,0 @@
-package com.example.dell.lab4;
-
-/**
- * Created by Dell on 11/23/2017.
- */
-
-public class hhhh {
-    private String khsnh;
-}
--- app/src/androidTest/java/com/example/dell/lab4/lalala.java
+++
@@ -1,8 +0,0 @@
-package com.example.dell.lab4;
-
-/**
- * Created by Dell on 11/23/2017.
- */
-
-public class lalala {
-}
